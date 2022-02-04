47dbfd9243141fc32f1ba1054f8681d73a1380bf
erhankabuk <erhankabuk@hotmail.com>
2022-02-04 13:39:23 +0300

    4 Feb 2022

---
+++ 2.5SumLists/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._5SumLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( "Sum of two lists: "+SumOfLists(digitsToNumber(CreateLinkedList()), digitsToNumber(CreateLinkedList())));
+        }
+
+        static LinkedList<int> CreateLinkedList()
+        {
+            Random random = new Random();
+            LinkedList<int> list = new LinkedList<int>();
+            for (int i = 0; i < 3; i++)
+            {
+                list.AddLast(random.Next(0, 10));
+            }
+            return list;
+        }
+
+        static int digitsToNumber(LinkedList<int> input)
+        {
+            string number = "";
+            for (int i = input.Count-1; i >=0; i--)
+            {
+                number += input.ElementAt(i);
+            }
+            WriteLinkedList(input);           
+            Console.WriteLine(number);
+            return int.Parse(number);
+        }
+        static int SumOfLists(int input1,int input2)
+        {           
+            return input1+input2;
+        }
+
+
+        static void WriteLinkedList(LinkedList<int> input)
+        {
+           
+            foreach (var item in input)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+        }
+
+
+
+    }
+}
