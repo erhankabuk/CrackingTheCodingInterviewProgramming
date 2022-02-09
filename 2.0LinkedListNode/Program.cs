3b65252f2f28a89875bfc36812a592ddffd1d16d
erhankabuk <erhankabuk@hotmail.com>
2022-02-09 22:06:20 +0300

    9 Feb 2022

---
+++ 2.0LinkedListNode/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2._0LinkedListNode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LinkedList<int> list = CreateLinkedList();
+            //Create and add Link
+            LinkedListNode<int> node = new LinkedListNode<int>(0);
+            list.AddLast(node);
+
+            WriteLinkedList(list);
+            Console.WriteLine(node.Previous.Value);
+
+
+        }
+
+        
+        static LinkedList<int> CreateLinkedList()
+        {
+            Random random = new Random();
+            LinkedList<int> list = new LinkedList<int>();
+          
+            for (int i = 0; i < 20; i++)
+            {
+                list.AddLast(random.Next(0, 10));
+            }
+            return list;
+        }
+
+        static void WriteLinkedList(LinkedList<int> input)
+        {
+
+            foreach (var item in input)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ 2.7Intersection/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2._7Intersection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 3, 1, 5, 9, 7, 2, 1 };
+            int[] arr2 = { 4, 6, 7, 2, 1 };
+
+            Console.WriteLine(CheckIntersection(arr1, arr2));
+
+        }
+
+        static bool CheckIntersection(int[] arr1, int[] arr2)
+        {
+
+            LinkedList<int> list1 = new LinkedList<int>(arr1);
+            LinkedList<int> list2 = new LinkedList<int>(arr2);
+            LinkedListNode<int> node1 = new LinkedListNode<int>(0);
+            LinkedListNode<int> node2 = new LinkedListNode<int>(0);
+            list1.AddLast(node1);
+            list2.AddLast(node2);
+
+            while (node1 != null || node2 != null)
+            {
+                //if (node1.Previous.Value != node2.Previous.Value) return false;
+                //else if (node1.Previous.Previous.Value == node2.Previous.Previous.Value) return true;
+                if (node1.Previous.Value == node2.Previous.Value) return true;
+                else return false;
+            }
+
+            return false;
+        }
+
+    }
+}
