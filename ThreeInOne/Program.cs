49369456559c38aaba15d35fa2874a42170cf8c3
erhankabuk <erhankabuk@hotmail.com>
2022-02-11 23:27:45 +0300

    11 Feb 2022

--- ThreeInOne/Program.cs
+++ ThreeInOne/Program.cs
@@ -8,18 +8,12 @@ namespace ThreeInOne
     {
         static void Main(string[] args)
         {
+            int[] array = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 }; 
 
-            int[] array = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
-            int length = array.Length;
-            int n;
-            if (length % 3 == 0) n = length / 3;
-            else n = (length / 3) + 1;
+            //Brute force
+            //CreateStakes(array, GetLength(array));
 
-            CreateStakes(array, n);
-
-            Console.WriteLine("Hello World!");
         }
-
         public static void CreateStakes(int[] array, int n)
         {
             Stack<int> stack1 = new Stack<int>();
@@ -36,6 +30,15 @@ namespace ThreeInOne
             Console.WriteLine(string.Join(" ", stack2));
             Console.WriteLine(string.Join(" ", stack3));
 
+        }
+
+        static int GetLength(int[] array)
+        {
+            int length = array.Length;
+            int n;
+            if (length % 3 == 0) n = length / 3;
+            else n = (length / 3) + 1;
+            return n;
         }
     }
 }
