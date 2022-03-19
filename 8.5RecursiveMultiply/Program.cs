9d7b0869819dd7185ea384d09dd77c808ce1eebd
erhankabuk <erhankabuk@hotmail.com>
2022-03-19 13:05:23 +0300

    19 Mar 2022

---
+++ 8.5RecursiveMultiply/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _8._5RecursiveMultiply
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 5;
+            Console.WriteLine(GetSumOfAll(a, b));
+            Console.ReadKey();
+        }
+
+        public static int GetSumOfAll(int a, int b)
+        {
+            int smaller = a < b ? a : b;
+            int bigger = a > b ? a : b;
+
+            if (smaller == 0) return 0;
+            else if (smaller == 1) return bigger;
+
+            //int halfOfSmaller = smaller >>1;//Divide by 2
+            int halfOfSmaller = smaller / 2;
+            int sumOfHalf = GetSumOfAll(halfOfSmaller, bigger);
+            return smaller % 2 == 0 ? sumOfHalf + sumOfHalf : sumOfHalf + sumOfHalf + bigger;
+
+        }
+
+
+    }
+}
